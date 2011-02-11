8df1b87554bf6763081ff0ef154efa9dd970e06f
Austin Zach <austinmzach@gmail.com>
2011-02-10 23:13:29 -0600

    added line break
    for sure

--- deployer.php
+++ deployer.php
@@ -16,8 +16,11 @@
 		mysql_connect($hostname, $username, $password) or die("Error: " . mysql_error());
 		mysql_select_db($dbname);
 		
-		$query = "insert into json_test(json_data) values('" . implode(",", $_POST) . "')";
-		mysql_query($query) or die("what and ID10T");
+		$json = json_decode($_POST);
+		foreach ($json->{'commits'} as $commit) {
+			$query = "insert into json_test(json_data) values('" . implode(",", $commit) . "')";
+			mysql_query($query) or die("what and ID10T");
+		}
 	} else { ?>
 		<html>
 			<head>
@@ -29,6 +32,58 @@
 					<input type="hidden" name="choking" value="no" />
 					<input type="submit" value="deploy" />
 				</form>
+				<?php
+				 
+					$jsonJunk = '{
+					  "before": "5aef35982fb2d34e9d9d4502f6ede1072793222d",
+					  "repository": {
+					    "url": "http://github.com/defunkt/github",
+					    "name": "github",
+					    "description": "Youre lookin at it.",
+					    "watchers": 5,
+					    "forks": 2,
+					    "private": 1,
+					    "owner": {
+					      "email": "chris@ozmm.org",
+					      "name": "defunkt"
+					    }
+					  },
+					  "commits": [
+					    {
+					      "id": "41a212ee83ca127e3c8cf465891ab7216a705f59",
+					      "url": "http://github.com/defunkt/github/commit/41a212ee83ca127e3c8cf465891ab7216a705f59",
+					      "author": {
+					        "email": "chris@ozmm.org",
+					        "name": "Chris Wanstrath"
+					      },
+					      "message": "okay i give in",
+					      "timestamp": "2008-02-15T14:57:17-08:00",
+					      "added": ["filepath.rb"],"modified":["deployer.php"]
+					    },
+					    {
+					      "id": "de8251ff97ee194a289832576287d6f8ad74e3d0",
+					      "url": "http://github.com/defunkt/github/commit/de8251ff97ee194a289832576287d6f8ad74e3d0",
+					      "author": {
+					        "email": "chris@ozmm.org",
+					        "name": "Chris Wanstrath"
+					      },
+					      "message": "update pricing a tad",
+					      "timestamp": "2008-02-15T14:36:34-08:00"
+					    }
+					  ],
+					  "after": "de8251ff97ee194a289832576287d6f8ad74e3d0",
+					  "ref": "refs/heads/master"
+					}';
+				
+					$json = json_decode($jsonJunk, true);
+					foreach ($json['commits'] as $commit) {
+						echo $commit['id'] . "<br />";
+						print_r($commit['modified']);
+						echo "<br />";
+						echo "<br />";
+						print_r($commit['added']); 
+					}
+				?>
 			</body>
 		</html>
 	<?php }
